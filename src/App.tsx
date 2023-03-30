e1ecfda88d5a37d51698239b5a73a8c860f2f39b
Ravi Tiwari <ravbi2007@gmail.com>
2023-03-30 20:34:01 +0545

    Handle correctly fetching channelId from url.

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const ChatWithProvider = () => {
   );
 
   useEffect(() => {
-    const channelId = pathname.replace("/", "");
+    const channelId = pathname.replaceAll("/", "");
     if (channelId) setChannelIdInUrl(channelId);
   }, [pathname]);
 
@@ -29,20 +29,19 @@ const ChatWithProvider = () => {
 };
 
 const url = window.location.href;
-const isLocalHost =
-  !!(
-    url.indexOf("localhost") > -1 ||
-    url === "[::1]" ||
-    url.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
-  );
-  // first one is for local and later one is for github pages.
-const basename = isLocalHost ? '' : '/one-day-chat';
+const isLocalHost = !!(
+  url.indexOf("localhost") > -1 ||
+  url === "[::1]" ||
+  url.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+);
+// first one is for local and later one is for github pages.
+const basename = isLocalHost ? "" : "/one-day-chat";
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <BrowserRouter basename={basename}>
-        <Routes >
+        <Routes>
           <Route path="*" element={<ChatWithProvider />} />
         </Routes>
       </BrowserRouter>
